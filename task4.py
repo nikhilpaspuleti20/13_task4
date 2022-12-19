9378e77ebd8b2e92728fb081761f624b670e91ee
nikhilpaspuleti20 <116423567+nikhilpaspuleti20@users.noreply.github.com>
2022-12-19 20:32:53 +0530

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,19 @@
+"""
+DAY:thursday
+DATE:8th dec 2022
+TOPIC:task
+AUTHOR:nikhil
+"""
+n=-1
+while(n<20):
+    n+=2
+    if n>8:
+        break
+    print(n)#1 3 5 7
+#2nd method
+n=0
+while(n<10):
+    n+=1
+    if n==4:
+        break
+print(n)#1 2 3
---
+++ task5.py
@@ -0,0 +1,9 @@
+"""
+DAY:thursday
+DATE:8th dec 2022
+TOPIC:task
+AUTHOR:nikhil
+AUTHOR:paspuleti
+"""
+#not to display continue
+n=0
